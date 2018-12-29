e31c929e7d196bc4978e81f7f34b11ad3a0b400b
Maicon Da Silva <maicon.dasilva@imaginecurve.com>
2018-12-29 00:20:17 +0000

    Added security group reference

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example1" {
-  ami           = "ami-e6768381"
-  instance_type = "t2.micro"
+  ami                    = "ami-e6768381"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+							#!/bin/bash
+							echo "Hello, World" > index.html
+							nohup busybox httpd -f -p 8080 &
+							EOF
 
   tags {
     Name = "terraform-example1"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example1-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
