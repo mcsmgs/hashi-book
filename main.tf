c5768843b50d39ca36e587324ee0fa46cf60a8a7
Maicon Da Silva <maicon.dasilva@imaginecurve.com>
2019-01-02 01:42:03 +0000

    First chapter of the book

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
+variable "server_port" {
+  description = "The port the server will use for http requests"
+  default     = 8080
+}
+
+data "aws_availability_zones" "all" {}
+
 provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "example1" {
+/*
+resource "aws_instance" "example-ec2" {
   ami                    = "ami-e6768381"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -14,17 +22,97 @@ resource "aws_instance" "example1" {
 							EOF
 
   tags {
-    Name = "terraform-example1"
+    Name = "terraform-example-ec2"
+  }
+}
+*/
+resource "aws_launch_configuration" "example-lc" {
+  image_id        = "ami-e6768381"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+							#!/bin/bash
+							echo "Hello, World" > index.html
+							nohup busybox httpd -f -p "${var.server_port}" &
+							EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example-asg" {
+  launch_configuration = "${aws_launch_configuration.example-lc.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.example-elb.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tags {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example-elb" {
+  name               = "terraform-asg-example-elb"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example1-instance"
+  name = "terraform-example-sc-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-sc-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_elb.example-elb.dns_name}"
 }
